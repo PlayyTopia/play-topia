697b88346e3ad6df7056bf709508e8e1916b42b5
majdiShomali <majdi.shomali.1997@gmail.com>
2023-07-07 00:22:56 +0300

    add to fav

--- client/src/componentes/Profile.js
+++ client/src/componentes/Profile.js
@@ -16,6 +16,7 @@ const Profile = () => {
 const { loading, data, error } = useSelector((state) => state.userNew);
 const [favGames, setFavGames]=useState(null)
 const [userId, setUserId]=useState(null)
+const [userData, setUserData]=useState(null)
 
   const dispatch = useDispatch();
 
@@ -24,10 +25,11 @@ const [userId, setUserId]=useState(null)
       dispatch(fetchUserNew(token))
       fetchDataFav(data[0]?._id)
       setUserId(data[0]?._id)
+      setUserData(data[0])
       console.log(data[0]?._id)
   }, [dispatch]);
 
-
+console.log(userData)
 const fetchDataFav = async(id) =>{
 
   try {
@@ -88,18 +90,123 @@ const fetchDataFav = async(id) =>{
   };
 
 
+//-------------ali--------------//
+
+const [editMode, setEditMode] = useState(false);
+const [userName, setUserName] = useState("ali");
+const [email, setEmail] = useState("aa@gmail.com");
 
 
-
+const toggleEditMode = () => {
+ setEditMode(!editMode);
+};
+const handleSubmit = (e) => {
+ e.preventDefault();
+ setEditMode(false);
+ Swal.fire({
+   title: 'Do you want to save the changes?',
+   showDenyButton: true,
+   showCancelButton: true,
+   confirmButtonText: 'Save',
+   customClass: {
+     container: 'my-swal-container',
+     title: 'my-swal-title',
+     confirmButton: 'my-swal-button',
+     denyButton: 'my-swal-button',
+     cancelButton: 'my-swal-button',
+   },
+   buttonsStyling: false,
+ }).then((result) => {
+   if (result.isConfirmed) {
+     Swal.fire('Saved!', '', 'success');
+   } else if (result.isDenied) {
+     Swal.fire('Changes are not saved', '', 'info');
+   }
+ });
+ 
+};
 
   return (
     <>
-      <div className="grid  lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 place-items-center p-5">
+
+<div className="max-w-screen-xl mx-auto px-4 md:px-8 text-white">
+      <div className="flex justify-between mt-4">
+        <div>
+          <div className="w-44 rounded-full" >
+            <img className="rounded-full" src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="a"/>
+          </div>
+          {editMode ? (
+            <div>
+            <lable className="me-5">update your name:</lable>
+            <input
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              placeholder="Enter your user name"
+              style={{ color: 'black' }}
+            /></div>
+            
+          ) : (
+            <h2 className="text-4xl">
+              Welcome, <span>{userName}</span>
+            </h2>
+          )}
+          <br/>
+          {editMode ? (
+            <div>
+            <lable className="me-5">update your email:</lable>
+            <input
+            placeholder="Enter your email"
+            className="mt-3"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ color: 'black' }}
+            />
+            </div>
+            
+          ) : (
+            <h3>Email: {email}</h3>
+          )}
+          {editMode ? (
+            <input type="file" />
+            ):("")}
+        </div>
+        <div>
+          {editMode ? (
+            <>
+                    
+              <Button className="p-1 me-2 h-10  border-solid border-[#E8AA42] border-2 text-[#E8AA42] hover:bg-[#E8AA42] hover:text-[#ffffff]" onClick={handleSubmit}>Save</Button>
+              <Button className="p-1 h-10  border-solid border-[#E8AA42] border-2 text-[#E8AA42] hover:bg-[#E8AA42] hover:text-[#ffffff]" onClick={toggleEditMode}>Cancel</Button>
+            </>
+          ) : (
+            <Button className="p-1 h-10 border-solid border-[#E8AA42] border-2 text-[#E8AA42] hover:bg-[#E8AA42] hover:text-[#ffffff]" onClick={toggleEditMode}>Edit</Button>
+          )}
+        </div>        </div>
+        <div>
+          <h3 className="text-2xl mt-7">Blogs:</h3>
+          <article className="flex w-full flex-col items-center text-center bg-gradient-small rounded-lg px-4 py-[4.75rem] border  svelte-1wk3997">
+            <h3 className="mb-2 font-semibold text-ondark-primary">Good job!</h3>{" "}
+            <p className="max-w-[19.5rem] text-sm">
+              there is no blogs here, you can add your own blog here
+            </p>{" "}
+          </article>
+        </div>
+        <div>
+          <h3 className="text-2xl mt-7">favorite games: </h3>
+          <article className="flex w-full flex-col items-center text-center bg-gradient-small rounded-lg px-4 py-[4.75rem] border  svelte-1wk3997">
+            {/* <h3 className="mb-2 font-semibold text-ondark-primary">Good job!</h3>{" "} */}
+           
+            {/* <p className="max-w-[19.5rem] text-sm">
+              there is no games here, you can add your favorite game here
+            </p>{" "} */}
+         
+            <div className="flex w-full justify-around flex-wrap">
         {favGames?.map((e) => {
           return (
             <div
               key={e._id}
-              className="relative w-72 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+              className="mb-5 relative w-72 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
             >
               {e.UsersIdFavorite.indexOf(userId) === -1 ? (
                 <Icon
@@ -171,6 +278,24 @@ const fetchDataFav = async(id) =>{
           );
         })}
       </div>
+
+
+
+
+
+
+
+          </article>
+
+
+
+
+          
+        </div>
+      </div>
+
+
+      
     </>
   );
 };
